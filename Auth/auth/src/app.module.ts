cf2423aa3489b0a51f4a82a6f1b6afdfd13f448c
Euwamahoro <e.uwamahoro@alustudent.com>
2024-10-31 02:38:15 +0200

    updated app module that connect to db and run my auth service.

--- Auth/auth/src/app.module.ts
+++ Auth/auth/src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserModule } from './user/user.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { connection } from 'mongoose'; // Import mongoose connection
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  async onModuleInit() {
+    // Log the connection status
+    connection.once('open', () => {
+      console.log('Connected to authors database');
+    });
+  }
+}

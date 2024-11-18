1fee3431653ce8a12000a1ff9ad93179b5fb10ff
Euwamahoro <e.uwamahoro@alustudent.com>
2024-11-19 01:57:05 +0200

    setting up mono-repo on my project

--- Auth/src/app.module.ts
+++ Auth/src/app.module.ts
@@ -1,34 +1,10 @@
-import { Module, OnModuleInit } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { GraphQLModule } from '@nestjs/graphql';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { ApolloFederationDriver, ApolloFederationDriverConfig } from '@nestjs/apollo';
-import { connection } from 'mongoose';
-import { ConfigModule } from '@nestjs/config';
-import {ClientsModule, Transport} from '@nestjs/microservices'
-import { join } from 'path';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI),
-    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
-      driver: ApolloFederationDriver,
-      autoSchemaFile: true,
-      playground: true,
-      introspection: true,
-    }),
-    UserModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements OnModuleInit {
-  async onModuleInit() {
-    connection.once('open', () => {
-      console.log('Connected to database');
-    });
-  }
-}
+export class AppModule {}
